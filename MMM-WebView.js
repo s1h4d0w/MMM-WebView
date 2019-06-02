8318c577e827c6cd4bc30fa576ca41e2cb67417f
Nick Betting <nickbetting@hotmail.com>
2019-06-03 01:24:35 +0200

    Add files via upload

---
+++ MMM-WebView.js
@@ -0,0 +1,36 @@
+/* Magic Mirror
+ * Module: MMM-WebView
+ *
+ * By lepel
+ * MIT Licensed.
+ */
+ 
+Module.register("MMM-WebView", {
+
+    defaults: {
+        url: "https://www.duckduckgo.com"
+		},
+
+    start: function() {
+        //var self = this;
+    },
+
+    getStyles: function() {
+       return ["modules/MMM-WebView/css/MMM-WebView.css"];
+    },
+
+    getDom: function() {
+
+      var wrapper = document.createElement("div");
+      wrapper.classList.add("wv-wrapper");
+      
+      var iframe = document.createElement("iframe");
+      iframe.setAttribute("src", url);
+      
+      wrapper.appendChild(iframe);
+      
+      return wrapper;
+      
+    },
+
+});
