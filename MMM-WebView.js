4f8059071c3350754edb173d480019f8920ddb75
Nick Betting <nickbetting@hotmail.com>
2019-06-03 19:43:53 +0200

    Added opacity option, removed unused code and added comments

--- MMM-WebView.js
+++ MMM-WebView.js
@@ -7,28 +7,41 @@
  
 Module.register("MMM-WebView", {
 
+    // defaults for module
     defaults: {
-        url: "https://www.duckduckgo.com"
+        url: "https://www.duckduckgo.com",
+        opacity: "1"
 		},
 
-    start: function() {
-        //var self = this;
-    },
-
+    // set CSS for module
     getStyles: function() {
        return ["modules/MMM-WebView/css/MMM-WebView.css"];
     },
 
+    // create module elements
     getDom: function() {
 
+      // create the containing div
       var wrapper = document.createElement("div");
+      // add class to containing div
       wrapper.classList.add("wv-wrapper");
       
+      // create iframe element
       var iframe = document.createElement("iframe");
+      // set url of iframe
       iframe.setAttribute("src", this.config.url);
+      // remove iframe frameborder
+      iframe.setAttribute("frameborder", "0");
+      // set opacity of iframe
+      iframe.setAttribute("style", "opacity: "+ this.config.opacity +";");
+      // set options for iframe element
+      iframe.setAttribute("allow", "encrypted-media");
+      iframe.setAttribute("allowtransparency", "true");
       
+      // add iframe to containing div
       wrapper.appendChild(iframe);
       
+      // return container and iframe
       return wrapper;
       
     },
